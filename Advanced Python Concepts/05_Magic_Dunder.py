2236e2e3f7a5e6b6d21325bdc98bdb844aee2bef
ainanax <catyjamei@gmail.com>
2026-02-21 11:16:50 +0800

    dunder

---
+++ Advanced Python Concepts/05_Magic_Dunder.py
@@ -0,0 +1,17 @@
+class Employee:
+    company = "HP"
+    
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    
+    def __str__(self):
+        return f"The name is {self.name} and the salary is {self.salary}"
+    
+    def __repr__(self):
+        return f"name: {self.name} \nsalary:{self.salary}"
+        
+e = Employee("Jamie", 105145)
+print(e.name,e.salary)
+print(str(e))
+print(repr(e))
